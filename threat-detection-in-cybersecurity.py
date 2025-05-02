48e8648eec3b7cab8bdaedb053d3dd038a5443bc
aamineaaziz <x24116181@student.ncirl.ie>
2025-05-02 21:03:19 +0100

    Update threat-detection-in-cybersecurity.py

--- threat-detection-in-cybersecurity.py
+++ threat-detection-in-cybersecurity.py
@@ -73,3 +73,7 @@ df_new
 
 df_new['label']=df[' Label']
 df_new['label']
+X1=df_new.iloc[:,:-1].values
+y1=df_new.iloc[:,-1].values
+
+X_train, X_test, y_train, y_test = train_test_split(X1, y1, test_size=0.3, random_state=42)
