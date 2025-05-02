97f024c241339deec889083ec67ddb870f32a0be
aamineaaziz <x24116181@student.ncirl.ie>
2025-05-02 21:05:13 +0100

    Update threat-detection-in-cybersecurity.py

--- threat-detection-in-cybersecurity.py
+++ threat-detection-in-cybersecurity.py
@@ -77,3 +77,23 @@ X1=df_new.iloc[:,:-1].values
 y1=df_new.iloc[:,-1].values
 
 X_train, X_test, y_train, y_test = train_test_split(X1, y1, test_size=0.3, random_state=42)
+# Scale numerical features
+numerical_cols = X.select_dtypes(include=['int64', 'float64']).columns
+X[numerical_cols] = scaler.fit_transform(X[numerical_cols])
+
+#this wasnt possible with those DataSets
+smote = SMOTE(random_state=42)
+X_smote, y_smote = smote.fit_resample(X, y)
+
+
+LR_model = LogisticRegression()
+
+# Train the model
+LR_model.fit(X_train, y_train)
+
+LR_y_pred = LR_model.predict(X_test)
+
+LR_y_prob = LR_model.decision_function(X_test)
+
+LR_roc_auc = roc_auc_score(y_test, LR_y_prob)
+print(f'ROC-AUC Score: {LR_roc_auc:.2f}')
